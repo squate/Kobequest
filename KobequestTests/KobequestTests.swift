cefd9b2c5c0332cfac35211f5d65fdb01fef3ba3
squate <tobogganlad@gmail.com>
2024-11-16 16:57:58 -0500

    Initial Commit

---
+++ KobequestTests/KobequestTests.swift
@@ -0,0 +1,17 @@
+//
+//  KobequestTests.swift
+//  KobequestTests
+//
+//  Created by Nate on 11/16/24.
+//
+
+import Testing
+@testable import Kobequest
+
+struct KobequestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
